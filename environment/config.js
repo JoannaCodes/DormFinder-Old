cff5c398e796bcb2de4cb23f6b0b702b517e8349
JoannaCodes <joannalara21@gmail.com>
2023-01-21 14:22:33 +0800

    Update config.js

--- environment/config.js
+++ environment/config.js
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBRFrT74RQxoCMo1RFSULpuz0g18LnyzLE",
   authDomain: "dormfinder-5e354.firebaseapp.com",
@@ -16,11 +11,12 @@ const firebaseConfig = {
   messagingSenderId: "152129696735",
   appId: "1:152129696735:web:ab504e02cb30fa728d8a6a",
   measurementId: "G-RK12X90BX6"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const storage = getStorage(app);
+if (!firebase.apps.length){
+   firebase.initializeApp(firebaseConfig);
+}
 
-export { db, storage };
+const db = firebase.firestore
+
+export { firebase , db }
